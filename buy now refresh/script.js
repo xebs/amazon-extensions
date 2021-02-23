d145bb26dbfbca67d705b627767c878588ff29b8
xebs <63508226+xebs@users.noreply.github.com>
2021-02-23 19:27:24 +0530

    Add files via upload

---
+++ buy now refresh/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("buy-now-button")){
+    document.getElementById("buy-now-button").click();
+} else if(document.getElementById("titleSection")){
+    location.reload();
+}
