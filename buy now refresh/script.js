7de10050c703682cfe54d8885cf2cfd8d67fb9d1
xebs <63508226+xebs@users.noreply.github.com>
2021-02-24 08:01:32 +0530

    Update script.js

--- buy now refresh/script.js
+++ buy now refresh/script.js
@@ -1,5 +1,21 @@
+var url = window.location.href
+var reload_time = 15
+
 if(document.getElementById("buy-now-button")){
     document.getElementById("buy-now-button").click();
 } else if(document.getElementById("titleSection")){
-    location.reload();
+    if(sessionStorage.getItem(url)){
+        let n = sessionStorage.getItem(url)
+        n = Number(n)
+        n = n+1
+        if (reload_time >= n){
+            sessionStorage.setItem(url, n.toString())
+            location.reload()
+        }
+    } else{
+        sessionStorage.setItem(url, "1")
+        if (reload_time >= 1){
+            location.reload()
+        }
+    }
 }
